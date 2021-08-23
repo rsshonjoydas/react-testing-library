acb00f0a3ac39d2cfc9d74370e385c2d00da795f
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-23 19:11:14 +0600

    better code

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,36 +1,46 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
+// ! getBy
+
+// ? getByText
 test('render header props', () => {
   render(<Header title="My Header" />);
   const headingElement = screen.getByText(/my header/i);
   expect(headingElement).toBeInTheDocument();
 });
 
+// ? getByRole single heading
 // test('render header props', () => {
 //   render(<Header title="My Header" />);
 //   const headingElement = screen.getByRole('heading');
 //   expect(headingElement).toBeInTheDocument();
 // });
 
+// ? getByRole multiple heading
 test('render header props', () => {
   render(<Header title="My Header" />);
   const headingElement = screen.getByRole('heading', { name: 'My Header' });
   expect(headingElement).toBeInTheDocument();
 });
 
+// ? getByTitle
 test('render header props', () => {
   render(<Header title="My Header" />);
   const headingElement = screen.getByTitle('Header');
   expect(headingElement).toBeInTheDocument();
 });
 
+// ? getByTestId
 test('render header props', () => {
   render(<Header title="My Header" />);
   const headingElement = screen.getByTestId('header-1');
   expect(headingElement).toBeInTheDocument();
 });
 
+// ! findBy
+
+// ? findByText
 test('render header props', async () => {
   render(<Header title="My Header" />);
   const headingElement = await screen.findByText(/my header/i);
