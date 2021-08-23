fa022174d8766b0f4f2df2867a186195c1404f1d
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-23 21:29:20 +0600

    multiple fireEvents

--- src/components/AddInput/__test__/AddInput.test.js
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -16,4 +16,13 @@ describe('AddInput', () => {
     fireEvent.change(inputElement, { target: { value: 'Go Grocery Shopping' } });
     expect(inputElement.value).toBe('Go Grocery Shopping');
   });
+
+  test('render input add button is clicked', () => {
+    render(<AddInput todos={[]} setTodos={mockedSetTodo} />);
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    const buttonElement = screen.getByRole('button', { name: /Add/i });
+    fireEvent.change(inputElement, { target: { value: 'Go Grocery Shopping' } });
+    fireEvent.click(buttonElement);
+    expect(inputElement.value).toBe('');
+  });
 });
